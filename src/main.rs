ee73d4d6f516ca0d3ac454bafc56c345cd32458d
Erik Garrison <erik.garrison@gmail.com>
2022-12-09 14:56:11 -0600

    report per-base alignment coverage in each node

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,46 @@ fn for_each_line_in_file(filename: &str, mut callback: impl FnMut(&str)) {
     }
 }
 
-fn for_each_step(line: &str, mut callback: impl FnMut(usize)) {
+fn for_each_step(line: &str,
+                 mut callback: impl FnMut(usize,usize),
+                 mut get_node_len: impl FnMut(usize) -> usize) {
     let walk = line.split('\t').nth(5).unwrap();
+    let target_start = line.split('\t').nth(7).unwrap().parse::<usize>().unwrap();
+    let target_end = line.split('\t').nth(8).unwrap().parse::<usize>().unwrap();
+    let target_len = target_end - target_start;
+    //println!("{}", line);
+    //println!("target_len = {}", target_len);
     if walk != "*" {
-        line.split('\t').nth(5).unwrap()
+        let fields = line.split('\t').nth(5).unwrap()
             .split(|c| { c == '<' || c == '>' })
             .filter(|s| { !s.is_empty() })
             .map(|s| { s.parse::<usize>().unwrap() })
-            .for_each(|i| { callback(i) });
+            .enumerate()
+            .collect::<Vec<(usize,usize)>>();
+        let mut seen: usize = 0;
+        let fields_len = fields.as_slice().len();
+        //println!("fields len = {}", fields_len);
+        for (i, j) in fields {
+            let mut len = get_node_len(j);
+            //println!("node {} len = {}", j, len);
+            if i == 0 {
+                //println!("on first step {} {} {}", len, target_start, seen);
+                assert!(len >= target_start);
+                len -= target_start;
+            }
+            if i == fields_len-1 {
+                //println!("on last step {} {} {}", len, target_end, seen);
+                assert!(target_end >= seen);
+                len = target_end - seen;
+            }
+            if i == fields_len {
+                assert!(false);
+            }
+            //println!("node {} adj len = {}", j, len);
+            seen += len;
+            callback(j, len);
+        }
+        //println!("seen = {}", seen);
     }
 }
 
@@ -50,21 +82,20 @@ fn main() {
     for_each_line_in_file(&args.alignments, |_l: &str| { lines += 1 });
     //println!("{} has {} nodes", args.alignments, lines);
     let mut coverage = vec![0; gfa.segments.len()];
-    for_each_line_in_file(&args.alignments, |l: &str| { for_each_step(l, |i| { coverage[i-1] += 1; }); });
+    for_each_line_in_file(&args.alignments, |l: &str| {
+        // get the start pos
+        for_each_step(
+            l,
+            |i, j| { coverage[i-1] += j; },
+            |id| { gfa.segments[id-1].sequence.len() }); });
     print!("#sample");
     for n in 1..gfa.segments.len()+1 {
         print!("\tnode.{}", n);
     }
     println!();
     print!("{}", args.alignments);
-    if args.len_scale {
-        for (i, v) in coverage.into_iter().enumerate() {
-            print!("\t{}", v * gfa.segments[i].sequence.len());
-        }
-    } else {
-        for v in coverage {
-            print!("\t{}", v);
-        }
+    for (i, v) in coverage.into_iter().enumerate() {
+        print!("\t{}", v as f64 / gfa.segments[i].sequence.len() as f64);
     }
     println!();
 }
